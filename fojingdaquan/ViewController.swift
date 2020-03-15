7fa5a63ed1396a577e66fb8f54d1002b14b9f92a
TangMonk <wytwyt02@gmail.com>
2020-03-15 15:06:56 +0800

    Initial Commit

---
+++ fojingdaquan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fojingdaquan
+//
+//  Created by tangmonk on 2020/3/15.
+//  Copyright © 2020 tangmonk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
